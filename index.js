bea0d7f958cabb3a42d53702c809a0f9b1c0cc8f
Kristina <kristin.petrivska@gmail.com>
2024-07-15 20:08:29 -0600

    edit code remove switch(numOrStr)

--- index.js
+++ index.js
@@ -15,20 +15,17 @@
 let numOrStr = prompt('input number or string');
 console.log(numOrStr);
 
-switch (numOrStr) {
+    
+switch(true){
     case null:
         console.log('ви скасували');
         break;
-    default: 
-        numOrStr = numOrStr.trim();
-        switch(true){
-            case (numOrStr === ''):
-                console.log('Empty String'); 
-                break; 
-            case (isNaN( +numOrStr )):
-                console.log('number is Ba_NaN');
-                break;
-            default:
-                console.log('OK!');
-        }
+    case (numOrStr.trim() === ''):
+        console.log('Empty String'); 
+        break; 
+    case (isNaN( +numOrStr )):
+        console.log('number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
 }
